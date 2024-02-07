d9e863d33ca19c1216241bd7203e0cbd1f4f3e35
Kalfin Syah Kilau Mayya <110911274+KalfinSyah@users.noreply.github.com>
2024-02-07 11:31:03 +0700

    Add files via upload

--- win11_toolbox.py
+++ win11_toolbox.py
@@ -1,5 +1,7 @@
 import ctypes
 import os
+import subprocess
+import re
 
 class Win11Toolbox:
     def is_admin(self):
@@ -21,18 +23,16 @@ class Win11Toolbox:
         os.system("start ms-settings:gaming-gamemode")
 
     def install_wsl(self):
-        user_input = str(input("\nAre you sure (y/n) : "))
-        
-        if user_input == "n":
-            return
-
         print("")
         os.system("wsl --install")
-    
+
     def upgrade_my_apps(self):
         print("")
         os.system("winget upgrade")
-        print("\nNB : Write --all for upgrade all your apps. If error occurred try use id instead of name. Write cencel for cenceling.")
+        print("\nHelp :")
+        print("--all = for upgrade all your apps.")
+        print("cencel = for cenceling.")
+        print("If error occurred try use id instead of name.")
         user_input = str(input(f"\nChoose one (by name/id) : "))
 
         if user_input == "cencel":
@@ -43,64 +43,85 @@ class Win11Toolbox:
         print("")
         os.system("pause")
 
-
-
 win11_toolbox = Win11Toolbox() 
 is_admin = win11_toolbox.is_admin()
+
+options = [
+    ["System Properties Performance", 
+    "Tweak your apperance of your windows, and some functionality"], 
+
+    ["Menage Startup Apps",
+    "Manage an apps that automatically start when your computer is on"],
+
+    ["Perform Cleanmgr", 
+    "Clear unnecessary files from your computer's hard disk"],
+
+    ["Game Mode",
+    "Turning things off in the background"],
+
+    ["Install WSL (Windows Subsystem for Linux)",
+    "Run native Linux command-line tools and software directly on Windows, without the need for virtual machines (VMs) or dual-boot setups"],
+
+    ["Upgrade My Apps",
+    "Upgrade your apps using winget"],
+
+    ["Exit", 
+    "Exit from this app"]
+]
 
 if is_admin:
     while True:
         os.system("cls")
-        print(f"\n- - - - - - - - - by kalfin syah - - - - - - - - - -")
-        print(f"\nAdmin privileges Status : {is_admin}")
-        print(f"\n- - - - - - - - - - - - - - - - - - - - - - - - - - -")
-        
-        print(f"\n- - - - - - - - - - - - - - - - - - - - - - - - - - -")
-        print("\nOptions : ")
-        print("\n1 = Tweak performance.")
-        print("\n2 = Menage startup apps (an apps that automatically start when your computer is on).")
-        print("\n3 = Perform cleanmgr (clear unnecessary files from your computer's hard disk).")
-        print("\n4 = Enable or Disable game mode.")
-        print("\n5 = Install WSL/Windows Subsystem for Linux (run native Linux command-line tools and software directly on Windows, without the need for virtual machines (VMs) or dual-boot setups).")
-        print("\n6 = Upgrade my apps (this is using winget, you can also do it on your own if you want).")
-        print("\n99 = exit")
-        print(f"\n- - - - - - - - - - - - - - - - - - - - - - - - - - -")
-        options = str(input("\nYour options : "))
-        print(f"\n- - - - - - - - - - - - - - - - - - - - - - - - - - -")
+        print("\n- - - - - - -  program by Kalfin Syah - - - - - - -")
+        for i, j in enumerate(options, start=1):
+            print(f"\n{i} = {j[0]}")
+            print(f"Description : \n{j[1]}")
+        print("\n- - - - - - - - - - - - - - - - - - - - - - - - - -")
+        user_option = str(input("\nPick option : "))
 
-        print(f"\n- - - - - - - - - - - - - - - - - - - - - - - - - - -")
-        if options == "1":
-            print("\nBefore tweaking, here is recommended advice :")
-            print("\n(this is the best tweak for me, but for you it might be diffrent)") 
-            print("1. Pick 'custom'")
-            print("2. Un-checklist all, except the 'smooth edges of screen fonts'\n")
-            os.system("pause")
-            win11_toolbox.open_SystemPropertiesPerformanceExe()
-        elif options == "2":
-            print("\nNB : Before you do something to your PC, make sure you know what you're doing!")
-            print("Hint : If you want 'best performance', turning off all startup apps might help.\n")
-            os.system("pause")
-            win11_toolbox.menage_autostart_programs()
-        elif options == "3":
-            print("\nCAUTIONS! When cleaning temporary files, as it might affect certain applications or functionalities.\n")
-            os.system("pause")
-            win11_toolbox.perform_cleanmgr()
-        elif options == "4":
-            print("\nOptimize your PC for gaming by turning on game mode!\n")
-            os.system("pause")
-            win11_toolbox.game_mode()
-        elif options == "5":
-            win11_toolbox.install_wsl()
-        elif options == "6":
-            win11_toolbox.upgrade_my_apps()
-        elif options == "7":
-            pass
-        elif options == "99":
+        os.system("cls")
+        if user_option == "1":
+            title = "System Properties Performance"
+            run_method = win11_toolbox.open_SystemPropertiesPerformanceExe
+        
+        elif user_option == "2":
+            title = "Manage Stratup Apps"
+            run_method =  win11_toolbox.menage_autostart_programs
+        
+        elif user_option == "3":
+            title = "Perform Cleanmgr"
+            run_method = win11_toolbox.perform_cleanmgr
+
+        elif user_option == "4":
+            title = "Game Mode"
+            run_method = win11_toolbox.game_mode
+
+        elif user_option == "5":
+            title = "Install WSL (Windows Subsystem for Linux)"
+            run_method = win11_toolbox.install_wsl
+
+        elif user_option == "6":
+            title = "Upgrade My Apps"
+            run_method = win11_toolbox.upgrade_my_apps
+
+        elif user_option == "7":
             os.system("cls")
             exit()
-        print(f"\n- - - - - - - - - - - - - - - - - - - - - - - - - - -")
+        else:
+            print("\nInvalid input!\n")
+            os.system("pause")
+            continue
 
-
+        print(f"\n- - - - - - - - {title} - - - - - - -")
+        user_sureness = str(input("\nContinue (y/n) ? "))
+        if user_sureness == "Y" or user_sureness == "y":
+            run_method()
+        elif user_sureness == "N" or user_sureness == "n":
+            continue
+        else:
+            print("\nInvalid input!\n")
+            os.system("pause")
+            continue
 
 else:
     os.system("cls")
