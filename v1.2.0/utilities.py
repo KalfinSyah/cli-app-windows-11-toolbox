79e6c470be8e1a696aa6c1dc36e7ee0b73a48041
Kalfin Syah Kilau Mayya <110911274+KalfinSyah@users.noreply.github.com>
2024-12-31 22:17:33 +0700

    Delete v1.2.0 directory

--- v1.2.0/utilities.py
+++
@@ -1,9 +0,0 @@
-from dependencies import os
-
-def clear_terminal():
-    os.system("cls")
-
-def show_error_with_pause(message=""):
-    print(f"\033[91m(Error) \033[93m{message}\033[0m\n")
-    os.system("pause")
-    return
